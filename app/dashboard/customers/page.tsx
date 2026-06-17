6ca5f69077ecb2020b589d5495d77b5cdbab1659
harsh8010-proag <harishwavare@gmail.com>
2026-06-17 16:15:26 +0530

    add some content

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import { lusitana } from '@/app/ui/font';
 const page = () => {
     return (
-        <div>Costmerrs pages</div>
+        <>
+            <div className={`${lusitana.className} text-xl`}>Costmerrs pages</div>
+            <p className='mt-5 text-gray-500'>i am traying to type in faster speed becose i wanna try to speed up fasrly that the resone of i try to type fast</p>
+        </>
     )
 }
 
